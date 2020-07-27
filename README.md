bca04a1581dc1076750b2b4ca5335a8370416742
Will Fawcett <wfawcett@gmail.com>
2020-07-26 18:23:11 -0600

    int

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# kitchen
+# gutenberg-reader
 
 ## Build Setup
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -70,16 +70,15 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+        light: {
+          primary: '#009688',
+          secondary: '#03a9f4',
+          accent: '#673ab7',
+          error: '#f44336',
+          warning: '#ff9800',
+          info: '#2196f3',
+          success: '#4caf50'
         }
       }
     }
